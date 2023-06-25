dba5bbc826f82553ca0167d3366412cb46059102
Leonel Caldera <leoandres146@gmail.com>
2023-06-25 00:57:57 -0300

    Movido hola.php a lib

--- lib/hola.php
+++ lib/hola.php
@@ -1,5 +1,5 @@
 <?php
-// Autor: Sergio Gómez
+// Autor: Sergio Gómez <sergio@uco.es>
 // El nombre por defecto es Mundo
 $nombre = isset($argv[1]) ? $argv[1] : "Mundo";
-@print "Hola, {$nombre}\n";
+@print "Hola, {$nombre}\n"; 
